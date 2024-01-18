daafb087ada29ad1b3901d3cd972df5793653535
Paramraj Singh Machre <81582257+bennytobby@users.noreply.github.com>
2024-01-18 17:30:34 +0530

    updated branch

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 /* All Processes */
 process.stdin.setEncoding("utf8");
 
-/* Imports */
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const dotenv = require('dotenv').config({ path: './.env' });
 
+/* MongoDB Connections */
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, './.env') })
+const uri = process.env.MONGO_CONNECTION_STRING;
+const databaseAndCollection = { db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION };
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+
+
+/* Prompting User Input */
 const portNumber = process.argv[2];
 const prompt = `Web server started running at http://localhost:3000\nStop to shutdown the server: `;
 process.stdout.write(prompt);
+
 
 /* Print out iteration */
 process.stdin.on("readable", function () {
@@ -24,3 +30,20 @@ process.stdin.on("readable", function () {
     process.stdout.write(prompt);
     process.stdin.resume();
 });
+
+
+/* All express */
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("views", path.resolve(__dirname, "templates"));
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(3000);
